f7aa61c9365da19e3612fb8332f677fed6e6474c
Korvyr <viusing@gmail.com>
2018-05-18 11:50:23 -0500

    Update dgvoice.go
    Increased frameRate to the max that a Discord channel can handle.

--- dgvoice.go
+++ dgvoice.go
@@ -29,7 +29,7 @@ import (
 // These below values seem to provide the best overall performance
 const (
 	channels  int = 2                   // 1 for mono, 2 for stereo
-	frameRate int = 48000               // audio sampling rate
+	frameRate int = 96000               // audio sampling rate
 	frameSize int = 960                 // uint16 size of each audio frame
 	maxBytes  int = (frameSize * 2) * 2 // max size of opus data
 )
